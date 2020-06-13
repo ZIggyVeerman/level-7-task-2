7e85240c98501e68c7b88cd75e88392020741a41
Ziggy Veerman <ziggy.veerman@hva.nl>
2020-06-13 16:32:06 +0200

    cleaned code up

--- app/src/main/java/com/example/hvaquest/fragments/LocationFragment.kt
+++ app/src/main/java/com/example/hvaquest/fragments/LocationFragment.kt
@@ -5,18 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
 import com.example.hvaquest.R
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
 class LocationFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_location, container, false)
@@ -25,8 +20,8 @@ class LocationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
+//        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+//            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+//        }
     }
 }
